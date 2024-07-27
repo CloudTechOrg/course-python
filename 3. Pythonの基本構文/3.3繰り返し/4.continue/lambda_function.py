c15f544a81c4f8fb8fbc0a7e73aba5b155977222
kymx1983 <kymx1983@gmail.com>
2024-07-27 20:19:52 +0900

    update indent

--- 3. Pythonの基本構文/3.3繰り返し/4.continue/lambda_function.py
+++ 3. Pythonの基本構文/3.3繰り返し/4.continue/lambda_function.py
@@ -1,7 +1,7 @@
 def lambda_handler(event, context):
     for i in range(10):
-    if i%2 == 0:
-            continue
+        if i%2 == 0:
+            continue # ループを継続
         print(i)
 
-    # 出力：1 3 5 7 9
+# 出力：1 3 5 7 9
