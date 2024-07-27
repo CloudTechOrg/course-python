395d011c104dc407129aaee9671818e434d05147
kymx1983 <kymx1983@gmail.com>
2024-07-27 20:40:53 +0900

    add lesson 3.5

---
+++ 3. Pythonの基本構文/3.5 データ構造/4.辞書/lambda_function.py
@@ -0,0 +1,5 @@
+def lambda_handler(event, context):
+    my_dict = {'apple': 1, 'banana': 2, 'orange': 3} # 辞書の定義
+    print(my_dict['apple'])  # 出力: 1
+    print(my_dict['banana'])  # 出力: 2
+    print(my_dict['orange'])  # 出力: 3
